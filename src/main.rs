cf71760d606944d55bb1691b4e8c6caae22fe618
Gabriel Alves <g04lves@gmail.com>
2022-11-29 13:05:26 -0800

    grep actually works valmir is really good with iterators

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,18 @@ fn main() -> Result<(), Error> {
     let args = Arguments::try_from(std_args)?;
     dbg!("{:?}", &args);
 
-    let file_contents = file_reader::read_file(&args.file_path)?;
+    let file_contents = grep(args)?;
     dbg!("{:?}", file_contents);
 
     Ok(())
+}
+
+fn grep(args: Arguments) -> Result<Vec<(usize, String)>, Error> {
+    let file_contents = file_reader::read_file(&args.file_path)?
+        .into_iter()
+        .enumerate()
+        .filter(|(_index, string)| string.contains(&args.regex))
+        .collect::<Vec<_>>();
+
+    Ok(file_contents)
 }
